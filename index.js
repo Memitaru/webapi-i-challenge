de872ccc2d7a9cc063627fdeb7fe8341a7a79b7b
Memitaru <amandatwah@gmail.com>
2019-05-08 17:18:44 -0700

    Set up single user get, still needs cannot find id error.

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ server.get('/api/users', (req, res) => {
         })
 })
 
+// Get user by id
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    
+        db.findById(id)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({error: "The user information could not be retrieved."})
+        })
+    
+})
+
 // Post a User
 
 server.post('/api/users', (req, res) => {
@@ -45,21 +60,7 @@ server.post('/api/users', (req, res) => {
     }
 })
 
-// server.post('/api/users', (req, res) => {
-//     const newUser = req.body;
-//     console.log(`Name: ${newUser.name} and Bio: ${newUser.bio}`)
-//     db.insert(newUser)
-//         .then(user => {
-//             if (user.name && user.bio){
-//                 res.status(201).json(user)
-//             } else {
-//                 res.status(400).json({errorMessage: "Please provide name and bio for the user."})
-//             }
-//         })
-//         .catch(err => {
-//             res.status(500).json({error: "There was an error while saving the user object to the database."})
-//         })
-// })
+
 
 // Listening
 
