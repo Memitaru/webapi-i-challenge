d0ee2b79270be5d875365db4ff8b875ea882b3ab
Memitaru <amandatwah@gmail.com>
2019-05-08 17:24:15 -0700

    Finished error for get request.

--- index.js
+++ index.js
@@ -35,7 +35,12 @@ server.get('/api/users/:id', (req, res) => {
     
         db.findById(id)
         .then(user => {
-            res.status(200).json(user)
+            if (user) {
+                res.status(200).json(user)
+            } else{
+                res.status(404).json({error: "The user with the specified ID does not exist."})
+            }
+            
         })
         .catch(err => {
             res.status(500).json({error: "The user information could not be retrieved."})
